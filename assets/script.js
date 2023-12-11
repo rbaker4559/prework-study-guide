46899280801ec75e7eee3b0d68bafff6bb5be8e2
Reginald Baker <reginaldbaker@Reginalds-MacBook-Pro.local>
2023-12-10 18:08:25 -0800

    testing

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,8 @@ listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
 
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
